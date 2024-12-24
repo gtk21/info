121f385cd5f3b10b1b506794880655c6027a8947
gtk <thirukumaran.g2022ai-ds@sece.ac.in>
2024-12-24 16:11:14 +0530

    24/12 Day 2 ~ Upto skills section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,26 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContents => {
+            tabContents.classList.remove('skills_activate')
+        })
+
+        target.classList.add('skills_activate')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('skills_activate')
+        })
+
+        tab.classList.add('skills_activate')
+    })
+})
 
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
