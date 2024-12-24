a7a6817e15813cc948c7ee39d322fd196d6b7a0c
gtk <thirukumaran.g2022ai-ds@sece.ac.in>
2024-12-24 23:10:09 +0530

    24/12 Day ~ 2(2nd Commit) upto work section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,12 +33,45 @@ tabs.forEach(tab => {
 
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work_container',{
+    selectors:{
+        target: '.work_card'
+    },
+    animation: {
+        duration: 300
+    }
+})
 
 /*===== Link Active Work =====*/
 
+const linkWork = document.querySelectorAll('.work_item')
+
+function activeWork(){
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=>l.addEventListener("click",activeWork))
 
 /*===== Work Popup =====*/
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("work_button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+});
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio_popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio_popup-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp_thumbnail img").src = portfolioItem.querySelector(".work_img").src;
+    document.querySelector(".portfolio_popup-subtitle span").innerHTML = portfolioItem.querySelector(".work_title").innerHTML;
+    document.querySelector(".portfolio_popup-body").innerHTML = portfolioItem.querySelector(".portfolio_item-details").innerHTML;
+}
 
 
 /*=============== SERVICES MODAL ===============*/
